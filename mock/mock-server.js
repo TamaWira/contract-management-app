b80f961887e634706a1011592a2cc94466d74177
Bayu Adhya <asusrog20012001@gmail.com>
2025-10-29 22:21:17 +0700

    feat: add mock server

---
+++ mock/mock-server.js
@@ -0,0 +1,59 @@
+import express, { json } from "express";
+import cors from "cors";
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+const contracts = [
+  {
+    id: 1,
+    avatarUrl:
+      "https://www.shutterstock.com/image-photo/happy-handsome-young-african-american-600nw-2432719071.jpg",
+    name: "Contract A",
+    description: "Description for Contract A",
+    status: "Signed",
+    date: new Date(),
+  },
+  {
+    id: 2,
+    avatarUrl:
+      "https://www.shutterstock.com/image-photo/happy-young-european-professional-business-260nw-2500948659.jpg",
+    name: "Contract A",
+    description: "Description for Contract A",
+    status: "Pending",
+    date: new Date(),
+  },
+  {
+    id: 3,
+    avatarUrl:
+      "https://www.shutterstock.com/image-photo/profile-picture-smiling-successful-young-260nw-2040223583.jpg",
+    name: "Contract A",
+    description: "Description for Contract A",
+    status: "Terminated",
+    date: new Date(),
+  },
+  {
+    id: 4,
+    avatarUrl:
+      "https://www.shutterstock.com/image-photo/profile-picture-smiling-successful-young-260nw-2040223583.jpg",
+    name: "Contract A",
+    description: "Description for Contract A",
+    status: "Terminated",
+    date: new Date(),
+  },
+];
+
+app.get("/api/contracts", (req, res) => {
+  res.json(contracts);
+});
+
+app.post("/api/contracts", (req, res) => {
+  const newContract = { id: contracts.length + 1, ...req.body };
+  contracts.push(newContract);
+  res.status(201).json(newContract);
+});
+
+app.listen(3001, () => {
+  console.log("Mock server running on http://localhost:3001");
+});
